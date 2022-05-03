e51e898505b5988c0bfde5a018a9e00b997fee72
Thais C Bertoldo <thaisbertoldo11@gmail.com>
2022-05-03 12:47:40 -0300

    fix: ajusta semantiva html

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,9 @@ import style from './Header.module.scss'
 
 export const Header: FunctionComponent = () => {
   return (
-    <header className={style['c-nav']}>
-      <img src="/images/logo.png" alt="logo" className={style['c-navbar__img']} />
-      <h1 className={style['c-navbar__title']}>Livraria</h1>
+    <header className={style['c-header']}>
+      <img src="/images/logo.png" alt="logo" className={style['c-header__img']} />
+      <h1 className={style['c-header__title']}>Livraria</h1>
     </header>
   )
 }
