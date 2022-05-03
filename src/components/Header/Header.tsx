afb04de6fe773dfc02da357a7cdaa4fce280c195
Thais C Bertoldo <thaisbertoldo11@gmail.com>
2022-05-03 12:46:46 -0300

    fix: ajusta element header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,9 @@ import style from './Header.module.scss'
 
 export const Header: FunctionComponent = () => {
   return (
-    <nav className={style['c-nav']}>
+    <header className={style['c-nav']}>
       <img src="/images/logo.png" alt="logo" className={style['c-navbar__img']} />
       <h1 className={style['c-navbar__title']}>Livraria</h1>
-    </nav>
+    </header>
   )
 }
